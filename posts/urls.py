4f223df1086dc45eeb50817d62ec4f57ce6127b8
Robin <robin.guesdon@gmail.com>
2022-06-09 14:54:14 +0200

    Ajout de la vue et du template de détail blog post

--- posts/urls.py
+++ posts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), # name='post_list' --> nom de l'URL qui sera utilisée afin d'identifier la vue
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'posts/post_list.html', {'posts' : posts})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
