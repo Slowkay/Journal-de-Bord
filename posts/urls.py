0483353b606e86874f63dfdcc27720cd6df85582
Robin <robin.guesdon@gmail.com>
2022-06-10 14:45:45 +0200

    Nettoyage du repos

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 from . import views
 
+"""
+L11 : 'post/<int:pk>/edit/' : Partie de l'URL à laquelle renvoit cette view 
+ex : https://robinguesdon-blog/post/2/edit/ (ici 2 correspond à la clé primaire de l'article du blog qu'on veut éditer)
+
+'views.post_edit' : Correspond à la view qui va être utilisée pour afficher cette page. 
+Ex : 'def post_edit()' dans le fichier views.py
+
+name='post_edit' : Nom que l'on donne à ce path(chemin). On s'en servira pour construire une URL dans un template. 
+Ex : <h1><a href="{% url 'post_list' %}">Django Girls Blog</a></h1>
+"""
+
 urlpatterns = [
-    path('', views.post_list, name='post_list'), # name='post_list' --> nom de l'URL qui sera utilisée afin d'identifier la vue
+    path('', views.post_list, name='post_list'), 
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
--- posts/views.py
+++ posts/views.py
@@ -5,13 +5,13 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 
-# Create your views here.
+
 def post_list(request):
     posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'posts/post_list.html', {'posts' : posts})
 
 def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+    post = get_object_or_404(Post, pk=pk) # Eviter d'afficher une page vide
     return render(request, 'posts/post_detail.html', {'post': post})
 
 def post_new(request):
