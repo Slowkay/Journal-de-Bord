32c2f206c304ef780ccbfd451caeb67b83e4713d
Robin <robin.guesdon@gmail.com>
2022-06-13 11:30:44 +0200

    Create folder model

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Post
+from .models import Folder, Post
 
 # Pour que le modèle soit visible dans l'interface d'administration
 admin.site.register(Post)
+admin.site.register(Folder)
 
